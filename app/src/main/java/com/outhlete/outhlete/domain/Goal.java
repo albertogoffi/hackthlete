6c42f5f000c491fbb0efe1c40cd53361f2efa27d
danydunk <daniele.zuddas@usi.ch>
2018-04-28 21:32:40 +0200

    added descriptions in Goals

--- app/src/main/java/com/outhlete/outhlete/domain/Goal.java
+++ app/src/main/java/com/outhlete/outhlete/domain/Goal.java
@@ -1,15 +1,26 @@
 package com.outhlete.outhlete.domain;
 
 public enum Goal {
-    COOL_DOWN,
-    STRETCHING_TOP_BODY,
-    STRETCHING_LOW_BODY,
-    STRETCHING_CORE_BODY,
-    CARDIO,
-    WARM_UP,
-    ABS,
-    LEGS,
-    CHEST,
-    ARMS,
-    BACK
+    COOL_DOWN("Cool down", "Full body"),
+    STRETCHING_TOP_BODY("Stretching", "Lower body"),
+    STRETCHING_LOW_BODY("Stretching", "Upper body"),
+    STRETCHING_CORE_BODY("Stretching", "Core"),
+    CARDIO("Cardio", "Full body"),,
+    WARM_UP ("Warm up", "Full body"),
+    ABS ("Muscle growth", "Abs"),
+    LEGS("Muscle growth", "Legs"),
+    CHEST("Muscle growth", "Chest"),
+    ARMS("Muscle growth", "Arms"),
+    BACK("Muscle growth", "Back");
+
+    private String description;
+    private String bodyPart;
+
+    Goal(String description, String bodyPart){
+        this.description = description;
+        this.bodyPart = bodyPart;
+    }
+
+
+
 }
