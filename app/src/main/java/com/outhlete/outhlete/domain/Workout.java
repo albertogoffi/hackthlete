9879b7dd467080302f7f4e2fcf1fe622a35fe36c
danydunk <daniele.zuddas@usi.ch>
2018-04-28 20:07:01 +0200

    fix to handle PubliBikeExercise in Workout

--- app/src/main/java/com/outhlete/outhlete/domain/Workout.java
+++ app/src/main/java/com/outhlete/outhlete/domain/Workout.java
@@ -1,12 +1,21 @@
 package com.outhlete.outhlete.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Workout {
     private final List<Exercise> exercises;
 
     public Workout(final List<Exercise> exercises) {
-        this.exercises = exercises;
+
+        this.exercises = new ArrayList<>();
+        for(Exercise exercise:exercises){
+            if(exercise instanceof PubliBikeExercise){
+                this.exercises.addAll(((PubliBikeExercise) exercise).getSegmentExercises());
+            }else{
+                this.exercises.add(exercise);
+            }
+        }
     }
 
     public int getTotalDuration() {
