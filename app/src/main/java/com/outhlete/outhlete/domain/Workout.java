0ff56f257be2e3fff2baf578e08ecb7f4b90a112
danydunk <daniele.zuddas@usi.ch>
2018-04-29 14:13:41 +0200

    added address to running exercises

--- app/src/main/java/com/outhlete/outhlete/domain/Workout.java
+++ app/src/main/java/com/outhlete/outhlete/domain/Workout.java
@@ -1,27 +1,53 @@
 package com.outhlete.outhlete.domain;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Workout {
     private final List<Exercise> exercises;
 
     public Workout(final List<Exercise> exercises) {
 
+        GeoApiContext geoApiContext = new GeoApiContext.Builder()
+                .queryRateLimit(3)
+                .apiKey("AIzaSyBq6e5OnxObqIWurfzay99fkCZQDvsVjOU")
+                .connectTimeout(1, TimeUnit.SECONDS)
+                .readTimeout(1, TimeUnit.SECONDS)
+                .writeTimeout(1, TimeUnit.SECONDS)
+                .build();
+
+
         this.exercises = new ArrayList<>();
         for (Exercise exercise : exercises) {
             if (exercise instanceof PubliBikeExercise) {
-                this.exercises.addAll(((PubliBikeExercise) exercise).getSegmentExercises());
+                for(Exercise segmentExercise:((PubliBikeExercise) exercise).getSegmentExercises()){
+                    this.exercises.add(addAddressToMovingExercises(geoApiContext, segmentExercise));
+                }
             } else {
-                this.exercises.add(exercise);
+                this.exercises.add(addAddressToMovingExercises(geoApiContext, exercise));
             }
         }
+    }
+
+    private Exercise addAddressToMovingExercises(GeoApiContext geoApiContext, Exercise exercise){
+        if(!exercise.getStartPosition().equals(exercise.getEndPosition())){
+            try {
+                com.google.maps.model.LatLng position = new com.google.maps.model.LatLng(exercise.getEndPosition().latitude, exercise.getEndPosition().longitude);
+                GeocodingResult reversedPosition = GeocodingApi.reverseGeocode(geoApiContext, position).await()[0];
+                String desc = exercise.getDescription()+reversedPosition.formattedAddress;
+                return new Exercise(exercise.getName(), desc, exercise.getStartPosition(), exercise.getEndPosition(),
+                        exercise.getDuration(), exercise.getImage(),exercise.getGoal());
+            }catch(Exception e){}
+        }else{
+            return exercise;
+        }
+        return null;
     }
 
     public int getTotalDuration() {
